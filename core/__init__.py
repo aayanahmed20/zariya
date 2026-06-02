248f665fee411df538f602bf9ee373b7c1a7e375
Aayan <aayanahmed2913@gmail.com>
2026-06-02 09:59:38 +0500

    feat: core/__init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,13 @@
 from .inference import get_engine, chat, stream
 from .memory import get_memory
+from .tts import get_tts
+from .summarizer import summarize_conversation, extract_key_points, generate_title
+from .flashcards import generate_flashcards, generate_flashcards_from_chat
 
-__all__ = ["get_engine", "chat", "stream", "get_memory"]
+__all__ = [
+    "get_engine", "chat", "stream",
+    "get_memory",
+    "get_tts",
+    "summarize_conversation", "extract_key_points", "generate_title",
+    "generate_flashcards", "generate_flashcards_from_chat",
+]
